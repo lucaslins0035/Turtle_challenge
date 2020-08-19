be30f361a2e1609cf8611e8340c2cb0da57a6411
lucaslins0035 <lucaslinssouza@gmail.com>
2020-08-19 16:08:59 -0300

    Add joystick pkg

---
+++ joystick/scripts/joystick.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import rospy
+from ds4_driver.msg import Status
+from geometry_msgs.msg import Twist
+
+def convert_to_cmd_vel(msg): 
+    pub = rospy.Publisher(rospy.get_param("/joystick/ds4_publish_in"), Twist, queue_size=10) 
+    resp = Twist()
+    l = 1; a = 1
+    resp.linear.x = msg.axis_left_y*l
+    resp.angular.z = msg.axis_right_x*a
+    pub.publish(resp)
+
+def subscriber():   
+    sub = rospy.Subscriber("/status",Status,convert_to_cmd_vel,queue_size=10)
+    rospy.spin()
+    
+
+if __name__ == "__main__":
+    rospy.init_node("ds4_converter")
+    rospy.loginfo("""
+    Use:
+    Left_Stick to move;
+    Right_Stick to turn;""")
+    while not rospy.is_shutdown():
+        subscriber()
